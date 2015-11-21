22b4c52262154f20d5c5f0713a2cf35f32a1286d
Gear61 <chessnone@yahoo.com>
2015-11-20 21:45:59 -0800

    Add flashcard sets page

--- app/src/main/java/com/randomappsinc/simpleflashcards/Activities/MainActivity.java
+++ app/src/main/java/com/randomappsinc/simpleflashcards/Activities/MainActivity.java
@@ -4,17 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.joanzapata.iconify.IconDrawable;
 import com.joanzapata.iconify.fonts.FontAwesomeIcons;
 import com.randomappsinc.simpleflashcards.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends StandardActivity {
+    @Bind(R.id.add_button) ImageView addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        addButton.setImageDrawable(new IconDrawable(this, FontAwesomeIcons.fa_plus)
+                .colorRes(R.color.white)
+                .actionBarSize());
     }
 
     @Override
