a4344f43d42af38d0a34a5dbde4cb87bb6f81d5a
Gear61 <chessnone@yahoo.com>
2015-11-24 23:47:07 -0800

    Load question in study mode and be able to go back and forth in set. No content logic

--- app/src/main/java/com/randomappsinc/simpleflashcards/Activities/StudyModeActivity.java
+++ app/src/main/java/com/randomappsinc/simpleflashcards/Activities/StudyModeActivity.java
@@ -1,24 +1,61 @@
 package com.randomappsinc.simpleflashcards.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.joanzapata.iconify.IconDrawable;
 import com.joanzapata.iconify.fonts.FontAwesomeIcons;
+import com.randomappsinc.simpleflashcards.Persistence.DataObjects.Flashcard;
+import com.randomappsinc.simpleflashcards.Persistence.DatabaseManager;
 import com.randomappsinc.simpleflashcards.R;
 
+import java.util.List;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
  * Created by alexanderchiou on 11/24/15.
  */
 public class StudyModeActivity extends StandardActivity {
+    @Bind(R.id.set_name) TextView flashcardSetTitle;
+    @Bind(R.id.no_flashcards) View noFlashcards;
+    @Bind(R.id.question_answer_pair) View qaPair;
+    @Bind(R.id.question) TextView question;
+
+    private List<Flashcard> flashcards;
+    private int currentPosition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.study_mode);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ButterKnife.bind(this);
+
+        String setName = getIntent().getStringExtra(MainActivity.FLASHCARD_SET_KEY);
+        flashcardSetTitle.setText(setName);
+        flashcards = DatabaseManager.get().getAllFlashcards(setName);
+        if (flashcards.size() == 0) {
+            qaPair.setVisibility(View.GONE);
+            noFlashcards.setVisibility(View.VISIBLE);
+        }
+        else {
+            noFlashcards.setVisibility(View.GONE);
+            qaPair.setVisibility(View.VISIBLE);
+        }
+
+        currentPosition = 0;
+        setUpFlashcard();
+    }
+
+    public void setUpFlashcard() {
+        Flashcard flashcard = flashcards.get(currentPosition);
+        question.setText(flashcard.getQuestion());
     }
 
     @Override
@@ -37,5 +74,35 @@ public class StudyModeActivity extends StandardActivity {
                         .colorRes(R.color.white)
                         .actionBarSize());
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+        }
+        switch (item.getItemId()) {
+            case R.id.random_flashcard:
+                break;
+            case R.id.previous_flashcard:
+                if (currentPosition == 0) {
+                    currentPosition = flashcards.size() - 1;
+                }
+                else {
+                    currentPosition--;
+                }
+                setUpFlashcard();
+                break;
+            case R.id.next_flashcard:
+                if (currentPosition == flashcards.size() - 1) {
+                    currentPosition = 0;
+                }
+                else {
+                    currentPosition++;
+                }
+                setUpFlashcard();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
