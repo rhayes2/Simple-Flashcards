77a7920ab8f2b500fad47b4c0c325da76b07de51
Gear61 <chessnone@yahoo.com>
2018-07-01 21:13:22 -0700

    Number of questions layout

--- app/src/main/java/com/randomappsinc/simpleflashcards/activities/QuizSettingsActivity.java
+++ app/src/main/java/com/randomappsinc/simpleflashcards/activities/QuizSettingsActivity.java
@@ -2,16 +2,24 @@ package com.randomappsinc.simpleflashcards.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.joanzapata.iconify.IconDrawable;
 import com.joanzapata.iconify.fonts.IoniconsIcons;
 import com.randomappsinc.simpleflashcards.R;
 import com.randomappsinc.simpleflashcards.constants.Constants;
+import com.randomappsinc.simpleflashcards.persistence.DatabaseManager;
+import com.randomappsinc.simpleflashcards.persistence.models.FlashcardSet;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class QuizSettingsActivity extends StandardActivity {
+
+    @BindView(R.id.num_questions) TextView numQuestions;
+
+    private FlashcardSet flashcardSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,11 @@ public class QuizSettingsActivity extends StandardActivity {
                 .setHomeAsUpIndicator(new IconDrawable(this, IoniconsIcons.ion_android_close)
                         .colorRes(R.color.white)
                         .actionBarSize());
+
+        int flashcardSetId = getIntent().getIntExtra(Constants.FLASHCARD_SET_ID_KEY, 0);
+        flashcardSet = DatabaseManager.get().getFlashcardSet(flashcardSetId);
+
+        numQuestions.setText(String.valueOf(flashcardSet.getFlashcards().size()));
     }
 
     @OnClick(R.id.start_quiz)
